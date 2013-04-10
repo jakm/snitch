227969f884715199482b71fa68334249912d0dba
Ronald Evers <evers@fox-it.com>
2013-04-10 17:53:10 +0200

    fix test

--- test_log2json.py
+++ test_log2json.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python
 
 import logging
+import sys
+
 from log2json import Log2Json
 
+stdout_handler = logging.StreamHandler(stream=sys.stdout)
+stdout_handler.setFormatter(Log2Json(project='phalanx'))
+
 logger = logging.getLogger('my logger')
+logger.addHandler(stdout_handler)
 logger.setLevel(logging.INFO)
 
 
 def tester():
-    Log2Json(project='phalanx', filename='phalanx.json')
+
 
     # test info
     logger.info('log2json initialised')
